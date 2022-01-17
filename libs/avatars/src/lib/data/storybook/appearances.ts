02912ab851db80670f649ba9c58de31e77aa173c
d3v0ps <aitorllj93@gmail.com>
2022-01-17 03:44:10 +0100

    fix(storybook): update storybook mocks with model changes

--- libs/avatars/src/lib/data/storybook/appearances.ts
+++ libs/avatars/src/lib/data/storybook/appearances.ts
@@ -1,17 +1,6 @@
 import type { Appearance } from '../../models/appearance';
+import { userAvatars } from '../mocks/user-avatars.data';
 
-export const gltfAppearanceMock: Appearance = {
-  format: 'gltf',
-  src: 'assets/avatars/samples/ed4aa425-9e13-4e51-9f2c-557dfe0db7ab/ed4aa425-9e13-4e51-9f2c-557dfe0db7ab.glb',
-  smallPreviewUrl: 'assets/avatar-144.png',
-  previewCamera: {
-    position: '0 1.5 0.7',
-    rotation: '-5 0 0',
-    scale: '0.4 0.4 0.4',
-  },
-};
+export const gltfAppearanceMock: Appearance = userAvatars[0].selectedAppearance;
 
-export const appearancesMocks: Appearance[] = [
-  gltfAppearanceMock,
-  gltfAppearanceMock,
-];
+export const appearancesMocks: Appearance[] = userAvatars[0].appearances;
--- libs/avatars/src/lib/data/storybook/avatars.ts
+++ libs/avatars/src/lib/data/storybook/avatars.ts
@@ -1,51 +1,10 @@
 import type { Avatar } from '../../models/avatar';
-import { appearancesMocks } from './appearances';
-import { scopesMocks } from './scopes';
-import { skillsMocks } from './skills';
+import { userAvatars } from '../mocks/user-avatars.data';
 
-export const avatarMock1: Avatar = {
-  id: '1',
-  welcomeMessage: 'Hello!',
-  name: 'John',
-  title: 'Software Engineer',
-  selectedAppearance: {
-    format: 'readyplayer.me',
-    src: 'assets/avatar-large.png',
-    smallPreviewUrl: 'assets/avatar-144.png',
-  },
-  appearances: appearancesMocks,
-  scopes: scopesMocks,
-  skills: skillsMocks,
-};
+export const avatarMock1: Avatar = userAvatars[0];
 
-export const avatarMock2: Avatar = {
-  id: '2',
-  welcomeMessage: 'Hello!',
-  name: 'John2',
-  title: 'Software Engineer',
-  selectedAppearance: {
-    format: 'readyplayer.me',
-    src: 'assets/avatar-large.png',
-    smallPreviewUrl: 'assets/avatar-144.png',
-  },
-  appearances: [],
-  scopes: [],
-  skills: [],
-};
+export const avatarMock2: Avatar = userAvatars[1];
 
-export const avatarMock3: Avatar = {
-  id: '3',
-  welcomeMessage: 'Hello!',
-  name: 'John3',
-  title: 'Software Engineer',
-  selectedAppearance: {
-    format: 'readyplayer.me',
-    src: 'assets/avatar-large.png',
-    smallPreviewUrl: 'assets/avatar-144.png',
-  },
-  appearances: [],
-  scopes: [],
-  skills: [],
-};
+export const avatarMock3: Avatar = userAvatars[3];
 
 export const avatarsMocks: Avatar[] = [avatarMock1, avatarMock2, avatarMock3];
