7d1ea35bc011eb7896def4a675195f5a9fe23c77
d3v0ps <aitorllj93@gmail.com>
2022-05-02 20:21:15 +0200

    temporary fix map loading

--- libs/worlds/src/lib/web-components/angular/world-map/world-map.component.ts
+++ libs/worlds/src/lib/web-components/angular/world-map/world-map.component.ts
@@ -71,9 +71,17 @@ export class WorldMapComponent {
 
     const protocol =
       document.URL.substring(0, 5) == 'http:' ? 'http:' : 'https:';
-    const url = `${protocol}//aitorllamas.com/Fantasy-Map-Generator/?maplink=https://cdn.jsdelivr.net/gh/d3v0ps/metaverse@main/apps/portal/src/assets/worlds/${
-      world.id
-    }.map&burg=${
+    // TODO: Replace this with .map file (not working right now)
+    // const url = `${protocol}//aitorllamas.com/Fantasy-Map-Generator/?options=default&maplink=${protocol}//aitorllamas.com/metaverse/apps/portal/assets/worlds/${
+    //   world.id
+    // }.map&burg=${
+    //   this.burg?.i || 0
+    // }&hideEditor=true&hideWelcomeMessage=true&presetStyle=${mapStyle}&preset=${
+    //   this.preset
+    // }`;
+    const url = `${protocol}//aitorllamas.com/Fantasy-Map-Generator/?seed=${
+      world.map?.info.seed
+    }&burg=${
       this.burg?.i || 0
     }&hideEditor=true&hideWelcomeMessage=true&presetStyle=${mapStyle}&preset=${
       this.preset
