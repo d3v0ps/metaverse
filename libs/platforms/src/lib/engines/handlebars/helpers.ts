b9b73ef62f197b2e46ab91a39e6616b6bacdd32f
d3v0ps <aitorllj93@gmail.com>
2022-05-08 12:36:49 +0200

    add more base helpers

--- libs/platforms/src/lib/engines/handlebars/helpers.ts
+++ libs/platforms/src/lib/engines/handlebars/helpers.ts
@@ -1,3 +1,5 @@
+import { AugmentedJSONSchema } from '../../languages/json/types/json-schema';
+
 export {
   camel as camelCase,
   constant as constantCase,
@@ -6,3 +8,15 @@ export {
   title as capitalCase,
   title as titleCase,
 } from 'case';
+
+export const pluralize = (word: string): string => `${word}s`;
+
+export const ifObject = (
+  schema: AugmentedJSONSchema,
+  { fn }: { fn: (params: any) => string }
+) => (schema.type === 'object' ? fn(schema) : '');
+
+export const ifEnum = (
+  schema: AugmentedJSONSchema,
+  { fn }: { fn: (params: any) => string }
+) => (schema.type === 'string' && schema.enum ? fn(schema) : '');
--- libs/platforms/src/lib/languages/json/types/json-schema.d.ts
+++ libs/platforms/src/lib/languages/json/types/json-schema.d.ts
@@ -1,1 +1,6 @@
 export { JSONSchema4 as JSONSchema } from 'json-schema';
+
+export type AugmentedJSONSchema = JSONSchema & {
+  name: string;
+  templateProperties: AugmentedJSONSchema[];
+};
